07c542c3a86ad1fdf3468a060395d9b51f2ac34e
Dae-woon Park <5886230+devvace@users.noreply.github.com>
2023-09-22 16:19:11 +0900

    Push 함수

--- lib/route/router.dart
+++ lib/route/router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:go_router_v7/screens/1_basic_screen.dart';
 import 'package:go_router_v7/screens/2_named_screen.dart';
+import 'package:go_router_v7/screens/3_push_screen.dart';
 import 'package:go_router_v7/screens/root_screen.dart';
 
 // https://knoow.tistory.com/ -> / -> path
@@ -23,6 +24,11 @@ final router = GoRouter(
             name: 'named_screen', // route가 길어졌을 때 유용함
             builder: (context, state) {
               return NamedScreen();
+            }),
+        GoRoute(
+            path: 'push',
+            builder: (context, state) {
+              return PushScreen();
             }),
       ],
     ),
