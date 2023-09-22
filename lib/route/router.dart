79231da9db62c3154746e7c4ac80de382807c6b1
Dae-woon Park <5886230+devvace@users.noreply.github.com>
2023-09-22 15:51:43 +0900

    Go 함수

--- lib/route/router.dart
+++ lib/route/router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:go_router_v7/screens/1_basic_screen.dart';
 import 'package:go_router_v7/screens/root_screen.dart';
 
 // https://knoow.tistory.com/ -> / -> path
@@ -9,7 +10,15 @@ final router = GoRouter(
       path: '/',
       builder: (context, state) {
         return RootScreen();
-      }
+      },
+      routes: [
+        GoRoute(
+          path: 'basic',
+          builder: (context, state) {
+            return BasicScreen();
+          }
+        )
+      ],
     ),
   ],
 );
