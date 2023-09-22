e6fd77eb6740cabc8b1dbb3039e5b67b179db277
Dae-woon Park <5886230+devvace@users.noreply.github.com>
2023-09-22 16:03:15 +0900

    Go Named 함수

--- lib/route/router.dart
+++ lib/route/router.dart
@@ -1,5 +1,6 @@
 import 'package:go_router/go_router.dart';
 import 'package:go_router_v7/screens/1_basic_screen.dart';
+import 'package:go_router_v7/screens/2_named_screen.dart';
 import 'package:go_router_v7/screens/root_screen.dart';
 
 // https://knoow.tistory.com/ -> / -> path
@@ -13,11 +14,16 @@ final router = GoRouter(
       },
       routes: [
         GoRoute(
-          path: 'basic',
-          builder: (context, state) {
-            return BasicScreen();
-          }
-        )
+            path: 'basic',
+            builder: (context, state) {
+              return BasicScreen();
+            }),
+        GoRoute(
+            path: 'named',
+            name: 'named_screen', // route가 길어졌을 때 유용함
+            builder: (context, state) {
+              return NamedScreen();
+            }),
       ],
     ),
   ],
